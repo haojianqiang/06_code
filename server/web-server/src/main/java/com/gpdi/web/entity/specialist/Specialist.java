575cf9abc930100a2ca0f31bf1440e3114f3048c
zhangzheng19951116 <2995483637@qq.com>
2019-11-22 08:57:04 +0800

    新增专家实体类

---
+++ server/web-server/src/main/java/com/gpdi/web/entity/specialist/Specialist.java
@@ -0,0 +1,11 @@
+package com.gpdi.web.entity.specialist;
+
+import java.io.Serializable;
+
+/**
+ * @Auther: zhangzheng
+ * @Date: 2019/11/22 08:54
+ * @Description:
+ */
+public class Specialist implements Serializable {
+}
