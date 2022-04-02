5671c31044795a7e880cd1171942e242bf412a26
grantchenhuarong <44857880+grantchenhuarong@users.noreply.github.com>
2022-04-02 16:10:45 +0800

    Update RevSMSServiceImpl.java

--- server/spring-cloud-schedule/src/main/java/com/gpdi/schedule/webservice/RevSMSServiceImpl.java
+++ server/spring-cloud-schedule/src/main/java/com/gpdi/schedule/webservice/RevSMSServiceImpl.java
@@ -1,7 +1,7 @@
 package com.gpdi.schedule.webservice;
 
-import com.gpdi.schedule.dao.SMSEchoDao;
-import com.gpdi.schedule.entity.sms.SMSEcho;
+import com.xxxx.schedule.dao.SMSEchoDao;
+import com.xxxx.schedule.entity.sms.SMSEcho;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +13,7 @@ import java.util.Date;
  * 与接口中指定的服务名称一致
  */
 @WebService(serviceName = "revSMS",
-        targetNamespace = "http://sms.gpdi.com",
+        targetNamespace = "http://sms.xxxx.com",
         endpointInterface = "com.gpdi.schedule.webservice.RevSMSService"
 )
 @Component
